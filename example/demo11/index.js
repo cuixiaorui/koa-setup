d506096f9e6624852b70129c74c915a272113061
cuixiaorui <cui_xiaorui@126.com>
2020-05-21 23:57:51 +0800

    refactor

---
+++ example/demo11/index.js
@@ -0,0 +1,33 @@
+const Koa = require("koa");
+
+const Router = require("koa-router");
+
+const serve = require("koa-static");
+
+const views = require("koa-views");
+
+const views = require("koa-body");
+
+const app = new Koa();
+
+app.use(serve(__dirname + "/static"));
+
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug",
+  })
+);
+
+app.use(
+  koaBody({
+    multipart: true,
+  })
+);
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.body = "hello demo11";
+});
+app.use(router.routes());
+
+app.listen(8080);
---
+++ example/demo4/index.js
@@ -0,0 +1,42 @@
+const Koa = require("koa")
+
+const Router = require("koa-router")
+
+
+const serve = require("koa-static")
+
+
+const views = require("koa-views")
+
+
+
+const views = require("koa-body")
+
+
+const app = new Koa()
+
+
+app.use(serve(__dirname+"/static"))
+
+
+
+app.use(views(__dirname+"/views",{
+    extension: "pug"
+}))
+
+
+
+app.use(koaBody({
+    multipart:true
+}))
+
+
+
+const router = new Router()
+router.get("/",(ctx)=>{
+    ctx.body = "hello demo4"
+})
+app.use(router.routes())
+
+
+app.listen(8080)
